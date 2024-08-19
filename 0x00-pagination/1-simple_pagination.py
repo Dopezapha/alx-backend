b91978647420c76466ff72587b17f371b60abfb0
Dopezapha <officialnwaneridaniel@gmail.com>
2024-08-20 01:59:53 +1000

    A method named get_page that takes two integer arguments page and page_size

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -19,8 +19,8 @@ def get_page(page: int, page_size: int, data: List[Any]) -> List[Any]:
     A method called get_page that takes two integers page and page_size
     and returns the appropriate page of the dataset
     """
-    assert isinstance(page, int) and page > 0,
-    assert isinstance(page_size, int) and page_size > 0,
+    assert isinstance(page, int) and page > 0
+    assert isinstance(page_size, int) and page_size > 0
 
     if page > len(data) // page_size + (1 if len(data) % page_size else 0):
         return []
