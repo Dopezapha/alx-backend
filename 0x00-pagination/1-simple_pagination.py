15c2665d0e788ec86f1e31ae0fb22f40e258640e
Dopezapha <officialnwaneridaniel@gmail.com>
2024-08-20 01:54:02 +1000

    A method named get_page that takes two integer arguments page and page_size

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -13,6 +13,7 @@ def index_range(page: int, page_size: int) -> tuple:
     end_index = page * page_size
     return (start_index, end_index)
 
+
 def get_page(page: int, page_size: int, data: List[Any]) -> List[Any]:
     """
     A method called get_page that takes two integers page and page_size
@@ -20,9 +21,9 @@ def get_page(page: int, page_size: int, data: List[Any]) -> List[Any]:
     """
     assert isinstance(page, int) and page > 0,
     assert isinstance(page_size, int) and page_size > 0,
-    
+
     if page > len(data) // page_size + (1 if len(data) % page_size else 0):
         return []
-    
+
     start, end = index_range(page, page_size)
     return data[start:end]
