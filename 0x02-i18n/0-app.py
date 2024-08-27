141427356726be34e807ae30b5fbf78b5723c13f
Dopezapha <officialnwaneridaniel@gmail.com>
2024-08-27 08:02:56 -0400

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,16 @@
+"""
+A Basic App
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Defines a function called index
+    """
     return render_template('0-index.html')
 
 if __name__ == '__main__':
